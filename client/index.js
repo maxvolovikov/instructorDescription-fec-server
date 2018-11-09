56d16da7a72c15bd58570065bab1f86885081e6d
Mikaylahasal <mikayla.k.hasal@gmail.com>
2018-11-09 15:52:27 -0800

    changes to css and indexjs for proxy

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './app.js';
 import React from 'react';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+// import Instructor from './components/instructor.js';
+// ReactDOM.render(<App />, document.getElementById('app'));
+window.Instructor = App;
